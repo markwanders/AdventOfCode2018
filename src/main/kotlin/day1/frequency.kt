806a7cb438a1c76d667cb90e20eacc628b12dc00
Mark Wanders <mndmwanders@gmail.com>
2018-12-01 11:53:59 +0100

    Day 1 second puzzle solution

--- src/main/kotlin/day1/frequency.kt
+++ src/main/kotlin/day1/frequency.kt
@@ -4,7 +4,8 @@ import java.io.File
 
 fun main(args : Array<String>) {
     val values = readFileLineByLineUsingForEachLine("src/main/resources/day1.txt")
-    println(values.sum())
+    solution1(values)
+    solution2(values)
 }
 
 fun readFileLineByLineUsingForEachLine(fileName: String) : IntArray {
@@ -13,4 +14,26 @@ fun readFileLineByLineUsingForEachLine(fileName: String) : IntArray {
        values.add(it.toInt())
     }
     return values.toIntArray()
+}
+
+fun solution1(values : IntArray) {
+    println(values.sum())
+}
+
+fun solution2(values: IntArray) {
+    var currentValue = 0
+    var historicValues: ArrayList<Int> = ArrayList(currentValue)
+    var repeat = false
+    while (!repeat) {
+        values.forEach {
+            currentValue += it
+            if (historicValues.contains(currentValue)) {
+                println("Found repetition of $currentValue")
+                repeat = true
+                return
+            } else {
+                historicValues.add(currentValue)
+            }
+        }
+    }
 }
