7b1b3e54339198e4b135ddfed2f48eafba044213
Mark Wanders <mndmwanders@gmail.com>
2018-12-02 12:26:44 +0100

    Day 2 second puzzle solution. Still not very pretty.

--- src/main/kotlin/day2/checksum.kt
+++ src/main/kotlin/day2/checksum.kt
@@ -5,10 +5,11 @@ import java.io.File
 fun main(args: Array<String>) {
     val values = day2.readFileLineByLineUsingForEachLine("src/main/resources/day2.txt")
     solution1(values)
+    solution2(values)
 }
 
 fun readFileLineByLineUsingForEachLine(fileName: String): Array<String> {
-    var values: ArrayList<String> = ArrayList()
+    val values: ArrayList<String> = ArrayList()
     File(fileName).forEachLine {
         values.add(it)
     }
@@ -33,4 +34,34 @@ fun solution1(input: Array<String>) {
     }
     val checksum = two * three
     println("Amount of twos: $two, amount of threes: $three, checksum: $checksum")
+}
+
+fun solution2(input: Array<String>) {
+    val charArrays = input.map { n -> n.toCharArray() }
+    var string1 = ""
+    var string2 = ""
+    var common = ""
+    for(charArray in charArrays) {
+        charArrays.forEach {
+            var i = 0
+            var diff = 0
+            while(i < charArray.size) {
+                if (charArray[i] != it[i]) {
+                    diff++
+                }
+                if(diff > 1) {
+                    break
+                }
+                i++
+            }
+            if(diff == 1) {
+                val commonList = ArrayList<Char>()
+                charArray.toCollection(commonList).retainAll(it.toCollection(ArrayList()))
+                string1 = String(charArray)
+                string2 = String(it)
+                common = String(commonList.toCharArray())
+            }
+        }
+    }
+    println("IDs which only differ one character: $string1 and $string2, common string: $common")
 }
