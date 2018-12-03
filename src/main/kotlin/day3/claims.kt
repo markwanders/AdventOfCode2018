cab50251a3f6c01148da0e359c4ba47cb8e5b598
Mark Wanders <mark.wanders@ing.com>
2018-12-03 11:57:57 +0100

    First star fir day 3

--- src/main/kotlin/day3/claims.kt
+++ src/main/kotlin/day3/claims.kt
@@ -1,7 +1,7 @@
 package day3
 
 import java.io.File
-import java.util.ArrayList
+import java.util.*
 
 fun main(args: Array<String>) {
     val values = readFileLineByLineUsingForEachLine("src/main/resources/day3.txt")
@@ -18,9 +18,17 @@ fun readFileLineByLineUsingForEachLine(fileName: String): Array<String> {
 
 fun solution1(input: Array<String>) {
     val claims = input.map { s -> Claim(s) }
+    var fabric = Array(1000, { IntArray(1000) })
     claims.forEach {
-        println("Claim: ${it.id}, ${it.startX}, ${it.startY}, ${it.x}, ${it.y}")
+        for(x in it.startX until it.startX + it.x) {
+            for(y in it.startY until it.startY + it.y) {
+                fabric[x][y] += 1
+            }
+        }
     }
+    var flattened = fabric.flatMap { it.asList() }
+    var count = flattened.count { it > 1 }
+    println("Amount of cells with more than one claim: $count")
 }
 
 class Claim(val id: Int, val startX: Int, val startY: Int, val x: Int, val y: Int) {
