15ecad822f7baeec5748ce8e9119d49518595ec2
Mark Wanders <mndmwanders@gmail.com>
2018-12-05 10:58:30 +0100

    Day 5 star 2

--- src/main/kotlin/day5/polymer.kt
+++ src/main/kotlin/day5/polymer.kt
@@ -5,6 +5,7 @@ import java.io.File
 fun main(args: Array<String>) {
     val polymer = readFile("src/main/resources/day5.txt")
     solution1(polymer)
+    solution2(polymer)
 }
 
 fun readFile(fileName: String): String {
@@ -12,17 +13,29 @@ fun readFile(fileName: String): String {
 }
 
 fun solution1(polymer: String){
-    val polymerArray = polymer.toMutableList()
+    println("Length of polymer after fully reacting is: ${react(polymer).length}")
+}
+
+fun solution2(polymer: String){
+    val polymerArray = polymer.toCharArray()
+    val minEntry = polymerArray.map { c -> c.toLowerCase() }.distinct().sorted().map { it -> it to react(String(polymerArray.filterNot { c -> c.toLowerCase() == it}.toCharArray()))}.minBy { it.second.length }
+    println("Removing letter ${minEntry!!.first} results in the shortest reacted polymer with length ${minEntry.second.length}.")
+}
+
+
+fun react(polymer: String) : String {
+    var polymerArray = polymer.toCharArray()
     do {
         var matched = false
         for(x in polymerArray.size - 1 downTo 1) {
             if(polymerArray[x] != polymerArray[x - 1] && (polymerArray[x] == polymerArray[x - 1].toLowerCase() || polymerArray[x] == polymerArray[x - 1].toUpperCase())) {
-                polymerArray.removeAt(x)
-                polymerArray.removeAt(x - 1)
+                polymerArray[x] = ' '
+                polymerArray[x - 1] = ' '
                 matched = true
             }
         }
+        polymerArray = String(polymerArray).replace("\\s".toRegex(), "").toCharArray()
     } while (matched)
 
-    println("Length of polymer after fully reacting is: ${polymerArray.size}")
+    return String(polymerArray)
 }
