9420a5d35e78e7a322c5fbc7f9a9f4df35665b91
Mark Wanders <mndmwanders@gmail.com>
2018-12-08 15:07:12 +0100

    Day 8 star 2

--- src/main/kotlin/day8/license.kt
+++ src/main/kotlin/day8/license.kt
@@ -5,6 +5,7 @@ import java.io.File
 fun main(args: Array<String>) {
     val input = readFile("src/main/resources/day8.txt")
     solution1(input)
+    solution2(input)
 }
 
 fun readFile(fileName: String): IntArray {
@@ -17,7 +18,39 @@ fun solution1(input: IntArray) {
     while (iterator.hasNext()) {
         sum += metadata(iterator)
     }
-    println(sum)
+    println("Metadata sum: $sum")
+}
+
+fun solution2(input: IntArray) {
+    var sum = 0
+    val iterator = input.iterator()
+    while (iterator.hasNext()) {
+        sum += value(iterator)
+    }
+    println("Root node value: $sum")
+}
+
+fun value(input: IntIterator): Int {
+
+    val children = input.nextInt()
+    val metadataEntries = input.nextInt()
+
+    val childValues = mutableMapOf<Int, Int>()
+    for (c in 0 until children) {
+        childValues[c] = value(input)
+    }
+
+    val valuesToUse = mutableListOf<Int>()
+    for (m in 0 until metadataEntries) {
+        valuesToUse.add(input.nextInt())
+    }
+
+    return if(children > 0) {
+        valuesToUse.map { it -> childValues[it - 1]?: 0  }.sum()
+    } else {
+        valuesToUse.sum()
+    }
+
 }
 
 fun metadata(input: IntIterator): Int {
