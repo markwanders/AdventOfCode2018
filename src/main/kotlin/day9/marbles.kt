9514875c85a0812225576905f31b80d7f8f89a74
Mark Wanders <mndmwanders@gmail.com>
2018-12-09 15:48:57 +0100

    Day 9 star 2

--- src/main/kotlin/day9/marbles.kt
+++ src/main/kotlin/day9/marbles.kt
@@ -1,10 +1,12 @@
 package day9
 
 import java.io.File
+import java.util.*
 
 fun main(args: Array<String>) {
     val input = readFile("src/main/resources/day9.txt")
-    solution1(input[0],input[1])
+    solution1(input[0], input[1])
+    solution2(input[0], input[1])
 }
 
 fun readFile(fileName: String): List<Int> {
@@ -14,35 +16,76 @@ fun readFile(fileName: String): List<Int> {
 
 fun solution1(numberOfPlayers: Int, numberOfMarbles: Int) {
     val players = IntArray(numberOfPlayers)
-    val playedMarbles = mutableListOf<Int>()
+    val playedMarbles = LinkedList<Int>()
     playedMarbles.add(0)
     var currentMarble = 0
     var currentPlayer = 0
-    for(x in 1 until numberOfMarbles + 1) {
+    (1 until numberOfMarbles + 1).forEach { marble ->
         var index = currentMarble + 2
-        if(index > (playedMarbles.size)) {
+        if (index > (playedMarbles.size)) {
             index -= (playedMarbles.size)
         }
-        if(x.rem(23) == 0) {
+        if (marble.rem(23) == 0) {
             var dropIndex = currentMarble - 7
-            if(dropIndex < 0) {
+            if (dropIndex < 0) {
                 dropIndex += playedMarbles.size
             }
-            players[currentPlayer] += x
+            players[currentPlayer] += marble
             players[currentPlayer] += playedMarbles[dropIndex]
             playedMarbles.removeAt(dropIndex)
             currentMarble = dropIndex
         } else {
-            playedMarbles.add(index, x)
+            playedMarbles.add(index, marble)
             currentMarble = index
         }
 
-        currentPlayer = if(currentPlayer == players.size - 1) {
+        currentPlayer = if (currentPlayer == players.size - 1) {
             0
         } else {
             currentPlayer + 1
         }
-
     }
-    println(players.max())
+    println("Highscore: ${players.max()} ")
 }
+
+fun solution2(numberOfPlayers: Int, numberOfMarbles: Int) {
+    val marbles = numberOfMarbles * 100
+    val players = LongArray(numberOfPlayers)
+    val playedMarbles = LinkedList<Long>()
+    playedMarbles.add(0)
+    var iterator = playedMarbles.listIterator()
+    var currentPlayer = 0
+    for (marble in 1 until marbles) {
+        when {
+            marble.rem(23) == 0 -> {
+                (0..7).onEach {
+                    if (iterator.hasPrevious()) {
+                        iterator.previous()
+                    } else {
+                        iterator = playedMarbles.listIterator(playedMarbles.size - 1)
+                    }
+                }
+                val drop = iterator.next()
+                iterator.remove()
+                players[currentPlayer] += (drop + marble)
+                iterator.next()
+            }
+            iterator.hasNext() -> {
+                iterator.next()
+                iterator.add(marble.toLong())
+            }
+            else -> {
+                iterator = playedMarbles.listIterator(1)
+                iterator.add(marble.toLong())
+            }
+        }
+        currentPlayer = if (currentPlayer == players.size - 1) {
+            0
+        } else {
+            currentPlayer + 1
+        }
+    }
+    println("Highscore: ${players.max()} ")
+}
+
+
