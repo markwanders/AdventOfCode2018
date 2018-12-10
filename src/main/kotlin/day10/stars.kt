0b963e352acfe8f1e17c9359f818a79b9c60c23a
Mark Wanders <mark.wanders@ing.com>
2018-12-10 13:31:04 +0100

    Day 10 stars 1 and 2

--- src/main/kotlin/day10/stars.kt
+++ src/main/kotlin/day10/stars.kt
@@ -1,11 +1,10 @@
 package day10
 
 import java.io.File
-import kotlin.math.absoluteValue
 
 fun main(args: Array<String>) {
-    val input = readFile("src/main/resources/test.txt")
-    solution1(input)
+    val input = readFile("src/main/resources/day10.txt")
+    solution1and2(input)
 }
 
 fun readFile(fileName: String): List<Pair<Pair<Int, Int>, Pair<Int, Int>>> {
@@ -17,33 +16,42 @@ fun readFile(fileName: String): List<Pair<Pair<Int, Int>, Pair<Int, Int>>> {
     return values.toList()
 }
 
-fun solution1(input: List<Pair<Pair<Int, Int>, Pair<Int, Int>>>) {
-    val state = input.toMutableList()
-    val maxX = input.maxBy { it.first.first }!!.first.first + 1
-    val minX = input.minBy { it.first.first }!!.first.first.absoluteValue
-    val x =  maxX + minX
-    val maxY = input.maxBy { it.first.second }!!.first.second + 1
-    val minY = input.minBy { it.first.second }!!.first.second.absoluteValue
-    val y = maxY + minY
-    val grid = Array(y) { CharArray(x) {'.'} }
-    repeat(4) {
-        val updated = mutableListOf<Pair<Int, Int>>()
-        state.forEachIndexed { index, old ->
-            if(!updated.contains(Pair(old.first.first + minX, old.first.second + minY))) {
-                grid[old.first.second + minY][old.first.first + minX] = '.'
-            }
-            val new = Pair(Pair((old.first.first + old.second.first), (old.first.second + old.second.second)), old.second)
-            state[index] = new
-            grid[new.first.second + minY][new.first.first + minX] = '#'
-            updated.add(Pair(new.first.first + minX, new.first.second + minY))
-        }.also {
-            grid.forEach {
-                it.forEach {
-                    print(it)
+fun solution1and2(input: List<Pair<Pair<Int, Int>, Pair<Int, Int>>>) {
+    var state = input.toMutableList()
+    var seconds = 0
+    do {
+        val oldXSize = state.maxBy { it.first.first }!!.first.first - state.minBy { it.first.first }!!.first.first
+        val oldYSize = state.maxBy { it.first.second }!!.first.second - state.minBy { it.first.second }!!.first.second
+
+        val newState = state.toMutableList()
+        newState.forEachIndexed { index, old ->
+            val new =
+                Pair(Pair((old.first.first + old.second.first), (old.first.second + old.second.second)), old.second)
+            newState[index] = new
+        }
+
+        val xSize = newState.maxBy { it.first.first }!!.first.first - newState.minBy { it.first.first }!!.first.first
+        val ySize = newState.maxBy { it.first.second }!!.first.second - newState.minBy { it.first.second }!!.first.second
+
+        if (oldXSize < xSize || oldYSize < ySize) {
+            val maxX = state.maxBy { it.first.first }!!.first.first + 1
+            val minX = state.minBy { it.first.first }!!.first.first
+            val maxY = state.maxBy { it.first.second }!!.first.second + 1
+            val minY = state.minBy { it.first.second }!!.first.second
+            val coords = state.map { it -> it.first }
+            for (y in minY until maxY) {
+                for (x in minX until maxX) {
+                    if (coords.contains(Pair(x, y))) {
+                        print('#')
+                    } else {
+                        print(' ')
+                    }
                 }
                 println()
             }
+            println("Read message after $seconds seconds")
         }
-    }
-
+        state = newState
+        seconds++
+    } while (xSize <= oldXSize && ySize <= oldYSize)
 }
