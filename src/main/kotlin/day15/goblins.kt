719eed9c905cfa728fce4fa39b550e66d82817fc
Mark Wanders <mndmwanders@gmail.com>
2018-12-15 21:41:59 +0100

    Find targets and free adjacent tiles

--- src/main/kotlin/day15/goblins.kt
+++ src/main/kotlin/day15/goblins.kt
@@ -1,7 +1,6 @@
 package day15
 
 import java.io.File
-import kotlin.math.absoluteValue
 
 val battlefield = readFile("src/main/resources/test.txt")
 val unitList = mutableListOf<Unit>()
@@ -19,20 +18,37 @@ fun readFile(fileName: String): MutableList<CharArray> {
 }
 
 fun solution1() {
+    battlefield.forEachIndexed { y,  line ->
+        line.forEachIndexed { x, char ->
+            if (char == 'E' || char == 'G') {
+                unitList.add(Unit(x, y, char))
+                battlefield[y][x] = '.'
+            }
+        }
+    }
+
     unitList.sortBy { it.x }
 
-    (0..5).forEach {
-        battlefield.forEachIndexed { y,  line ->
-            line.forEachIndexed { x, char ->
-                if(unitList.any { unit -> unit.x == x && unit.y == y }) {
-                    print(unitList.first {unit -> unit.x == x && unit.y == y  }.type)
-                } else {
-                    print(char)
+    (0..1).forEach {
+        print()
+        unitList.forEach {unit ->
+            println("Finding targets for ${unit.type} at ${unit.x}, ${unit.y}")
+            val targets = unitList.filter { enemy -> enemy.type != unit.type } //determine targets for each unit
+            targets.forEach {target -> //get adjacent, unoccupied tiles
+                val adjacentTiles = mutableListOf<Pair<Int, Int>>()
+                (target.y-1..target.y+1).forEach { y ->
+                        if(battlefield[y][target.x] == '.' && y != target.y) {
+                            adjacentTiles.add(Pair(target.x, y))
+                    }
                 }
+                (target.x-1..target.x+1).forEach { x ->
+                    if(battlefield[target.y][x] == '.' && x != target.x) {
+                        adjacentTiles.add(Pair(x, target.y))
+                    }
+                }
+                println("Found target ${target.type} at ${target.x}, ${target.y} with free adjacent tiles: $adjacentTiles")
             }
-            println()
         }
-        unitList.forEach { unit -> moveOneStepNearer(unit, findNearestEnemy(unit), battlefield) }
     }
 }
 
@@ -41,49 +57,16 @@ data class Unit(var x: Int, var y: Int, var type: Char) {
     val ap = 3
 }
 
-fun findNearestEnemy(unit: Unit) : Unit {
-    var grid = battlefield.toMutableList()
-    var simulatedUnit = unit.copy()
-    return unitList.filter { enemy -> enemy.type != simulatedUnit.type }.map { enemy ->
-        enemy to moveToEnemy(simulatedUnit, enemy, grid)
-    }.minBy { it.second }!!.first
-}
-
-fun moveToEnemy(unit: Unit, enemy: Unit, grid: MutableList<CharArray>) : Int {
-    var steps = 0
-    while ((unit.x - enemy.x).absoluteValue > 1 || (unit.y - enemy.y).absoluteValue > 1) {
-        moveOneStepNearer(unit, enemy, grid)
-        steps++
-    }
-    return steps
-}
-
-fun moveOneStepNearer(unit: Unit, enemy: Unit, grid: MutableList<CharArray>) {
-    grid[unit.x][unit.y] = '.'
-    val difX = unit.x - enemy.x
-    val difY = unit.y - enemy.y
-    if(difX.absoluteValue >= difY.absoluteValue) {
-        unit.x = if(difX > 0) {
-            var char = grid[unit.x -1][unit.y]
-            if(char == '.') {
-                unit.x - 1
+fun print() {
+    //Print battlefield
+    battlefield.forEachIndexed { y,  line ->
+        line.forEachIndexed { x, char ->
+            if(unitList.any { unit -> unit.x == x && unit.y == y }) {
+                print(unitList.first {unit -> unit.x == x && unit.y == y  }.type)
             } else {
-                unit.x
-            }
-        } else {
-            var char = grid[unit.x +1][unit.y]
-            if(char == '.') {
-                unit.x + 1
-            } else {
-                unit.x
+                print(char)
             }
         }
-    } else {
-        unit.y = if(difY > 0) {
-            unit.y - 1
-        } else {
-            unit.y + 1
-        }
+        println()
     }
-    grid[unit.x][unit.y] = unit.type
 }
