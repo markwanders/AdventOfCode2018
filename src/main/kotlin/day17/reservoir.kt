1736be4147b7e1ca34e39b0a7d24f6292ac1ac03
Mark Wanders <mark.wanders@ing.com>
2018-12-17 12:53:59 +0100

    Day 17 working for test input

--- src/main/kotlin/day17/reservoir.kt
+++ src/main/kotlin/day17/reservoir.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
     solution1(input)
 }
 
-fun readFile(fileName: String): Array<CharArray> {
+fun readFile(fileName: String): Array<Array<Char>> {
     val yRegex = "(?<=y=)(\\d*)".toRegex()
     val xRegex = "(?<=x=)(\\d*)".toRegex()
     val yRangeRegex = "(?<=y=)(\\d*)\\.\\.(\\d*)".toRegex()
@@ -33,7 +33,7 @@ fun readFile(fileName: String): Array<CharArray> {
             }
         }
     }
-    val grid = Array(sand.maxBy { it.second }!!.second + 1) { CharArray(2 * sand.maxBy { it.first }!!.first + 1) { '.'} }
+    val grid = Array(sand.maxBy { it.second }!!.second + 1) { Array(2 * sand.maxBy { it.first }!!.first + 1) { '.'} }
     sand.forEach {
         grid[it.second][it.first] = '#'
     }
@@ -41,11 +41,62 @@ fun readFile(fileName: String): Array<CharArray> {
     return grid
 }
 
-fun solution1(grid: Array<CharArray>) {
+fun solution1(grid: Array<Array<Char>>) {
     grid.forEach {
-        it.forEach {
-            print(it)
+        it.forEachIndexed { x, it ->
+            if (x in 491..509) print(it)
         }
         println()
     }
+    var water = mutableListOf<Pair<Int, Int>>()
+    water.add(500 to 0)
+    repeat(10) {
+
+        val addedWater = mutableListOf<Pair<Int, Int>>()
+        water.forEach {source ->
+            var y = source.second
+            while (y < grid.size - 1 && (grid[y + 1][source.first] == '.' || grid[y + 1][source.first] == '|')) {
+                y++
+                grid[y][source.first] = '|'
+            }
+            var x = source.first
+            while (x < grid[y].size - 1 && grid[y][x + 1] != '#') {
+                x++
+            }
+            val maxX = x
+            x = source.first
+            while (grid[y][x - 1] != '#' && x - 1 > 0) {
+                x--
+            }
+            val minX = x
+            if(y < grid.size - 1 && grid[y + 1].slice(minX..maxX).filterNot { tile -> tile == '#' || tile == '~' }.isEmpty()) { // fill with water
+                for(i in minX..maxX) {
+                    grid[y][i] = '~'
+                }
+            } else {
+                var xBottom = source.first
+                while (y < grid.size - 1 && grid[y][xBottom + 1] == '.' && grid[y + 1][xBottom] != '.') {
+                    xBottom++
+                    grid[y][xBottom] = '|'
+                }
+                addedWater.add(xBottom to y)
+                xBottom = source.first
+                while (y < grid.size - 1 && grid[y][xBottom - 1] == '.' && grid[y + 1][xBottom] != '.') {
+                    xBottom--
+                    grid[y][xBottom] = '|'
+                }
+                addedWater.add(xBottom to y)
+            }
+        }
+        water.addAll(addedWater)
+        water = water.distinct().toMutableList()
+        grid.forEach {line ->
+            line.forEachIndexed { x, char ->
+                if (x in 491..509) print(char)
+            }
+            println()
+        }
+        println()
+    }
+    println(grid.flatten().count { it == '~' }.plus(grid.flatten().count { it == '|' }))
 }
