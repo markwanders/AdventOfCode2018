4076220ad79017e0a3b2c04f355dc2ae0eeddeef
Mark Wanders <mark.wanders@ing.com>
2018-12-19 10:39:24 +0100

    Day 19 star 1

--- src/main/kotlin/day19/flow.kt
+++ src/main/kotlin/day19/flow.kt
@@ -11,10 +11,7 @@ val program = ArrayList<Pair<String, List<Int>>>()
 
 fun main(args: Array<String>) {
     readFile("src/main/resources/day19.txt")
-    println(ip)
-    program.forEach {
-        println(it)
-    }
+    solution1()
 }
 
 fun readFile(fileName: String) {
@@ -27,100 +24,109 @@ fun readFile(fileName: String) {
     }
     operations["addr"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]] + input[opcode[2]]
+        output[opcode[2]] = input[opcode[0]] + input[opcode[1]]
         return output
     }
     operations["addi"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]] + opcode[2]
+        output[opcode[2]] = input[opcode[0]] + opcode[1]
         return output
     }
     operations["mulr"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]] * input[opcode[2]]
+        output[opcode[2]] = input[opcode[0]] * input[opcode[1]]
         return output
     }
     operations["muli"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]] * opcode[2]
+        output[opcode[2]] = input[opcode[0]] * opcode[1]
         return output
     }
     operations["banr"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]] and input[opcode[2]]
+        output[opcode[2]] = input[opcode[0]] and input[opcode[1]]
         return output
     }
     operations["bani"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]] and opcode[2]
+        output[opcode[2]] = input[opcode[0]] and opcode[1]
         return output
     }
     operations["borr"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]] or input[opcode[2]]
+        output[opcode[2]] = input[opcode[0]] or input[opcode[1]]
         return output
     }
     operations["bori"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]] or opcode[2]
+        output[opcode[2]] = input[opcode[0]] or opcode[1]
         return output
     }
     operations["setr"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = input[opcode[1]]
+        output[opcode[2]] = input[opcode[0]]
         return output
     }
     operations["seti"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = opcode[1]
+        output[opcode[2]] = opcode[0]
         return output
     }
     operations["gtir"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = when {
-            opcode[1] > input[opcode[2]] -> 1
+        output[opcode[2]] = when {
+            opcode[0] > input[opcode[1]] -> 1
             else -> 0
         }
         return output
     }
     operations["gtri"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = when {
-            input[opcode[1]] > opcode[2] -> 1
+        output[opcode[2]] = when {
+            input[opcode[0]] > opcode[1] -> 1
             else -> 0
         }
         return output
     }
     operations["gtrr"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = when {
-            input[opcode[1]] > input[opcode[2]] -> 1
+        output[opcode[2]] = when {
+            input[opcode[0]] > input[opcode[1]] -> 1
             else -> 0
         }
         return output
     }
     operations["eqir"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = when {
-            opcode[1] == input[opcode[2]] -> 1
+        output[opcode[2]] = when {
+            opcode[0] == input[opcode[1]] -> 1
             else -> 0
         }
         return output
     }
     operations["eqri"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = when {
-            input[opcode[1]] == opcode[2] -> 1
+        output[opcode[2]] = when {
+            input[opcode[0]] == opcode[1] -> 1
             else -> 0
         }
         return output
     }
     operations["eqrr"] = fun(input: List<Int>, opcode: List<Int>): List<Int> {
         val output = input.toMutableList()
-        output[opcode[3]] = when {
-            input[opcode[1]] == input[opcode[2]] -> 1
+        output[opcode[2]] = when {
+            input[opcode[0]] == input[opcode[1]] -> 1
             else -> 0
         }
         return output
+    }
+}
+
+fun solution1() {
+    var input = mutableListOf(0,0,0,0,0,0)
+    while(input[ip] < program.size) {
+        input = operations[program[input[ip]].first]!!(input, program[input[ip]].second).toMutableList()
+        println(input)
+        input[ip]++
     }
 }
